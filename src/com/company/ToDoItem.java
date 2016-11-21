b385be3be26dc9ef1dba6420d8ed5ea43a7dda37
Matthew Scofield <matthewgscofield@outlook.com>
2016-11-21 14:17:45 -0600

    ToDo list exercise

---
+++ src/com/company/ToDoItem.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.security.PublicKey;
+
+/**
+ * Created by scofieldservices on 11/21/16.
+ */
+public class ToDoItem {
+    public String text;
+    public boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+
+
+}
